e7f10a7102fec36aa4a0f2bffb185f2de7e11177
Ferry Faisal <ferryuli@gmail.com>
2022-01-15 17:55:25 +0700

    initial commit

---
+++ src/notes.js
@@ -0,0 +1,14 @@
+const notes = [
+
+];
+
+/* {
+        id: String,
+        title: String,
+        createdAt: String,
+        updatedAt: String,
+        tags: array of String,
+        body: String,
+    }, */
+
+module.exports = notes;
